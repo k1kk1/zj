1bde17a790a66960099d568a7c8f4f53e192b4e9
kikkidev64 <kikkidev64@gmail.com>
2021-06-15 20:48:08 +0900

    first commit

---
+++ zj.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCHEDULE_JSON_PATH=$(pwd)/schedule.json
+if [[ ! -f $SCHEDULE_JSON_PATH ]]; then
+  echo "{\"items\":[{\"title\":\"test\",\"url\":\"https://hoge.zoom.us/j/1234567890?pwd=hogepass11Hoge\"}]}" | jq '.' > $SCHEDULE_JSON_PATH
+fi
+
+join() {
+  local re="https?://([a-zA-Z_0-9]+)\.zoom\.us/j/([0-9]+)\?pwd=([a-zA-Z_0-9]+)"
+  # TODO BASH_REMATCHでは(?: ... )といった記法が使えないため暫定的対応
+  local reNoPass="https?://([a-zA-Z_0-9]+)\.zoom\.us/j/([0-9]+)"
+  if [[ $1 =~ $re ]]; then
+    open zoommtg:"//${BASH_REMATCH[1]}.zoom.us/join?confno=${BASH_REMATCH[2]}&pwd=${BASH_REMATCH[3]}"
+  elif [[ $1 =~ $reNoPass ]]; then
+    open zoommtg:"//${BASH_REMATCH[1]}.zoom.us/join?confno=${BASH_REMATCH[2]}"
+  fi
+  echo "Domain : ${BASH_REMATCH[1]}"
+  echo "Join meeting - ID : ${BASH_REMATCH[2]}"
+}
+
+selectTitle () {
+  local title=$(cat $SCHEDULE_JSON_PATH | jq -r '.items[].title' | fzf --preview "cat $SCHEDULE_JSON_PATH | jq -r '.items[] | select(.title == \""{}"\")'")
+  local url=$(cat $SCHEDULE_JSON_PATH | jq -r ".items[] | select(.title == \""$title"\") | .url")
+  join $url
+}
+
+selectTitle
